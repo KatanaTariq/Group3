ef2e1a95f62b3e22ff7ecaabb55353feb9d18047
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2025-11-25 17:01:55 +0000

    Fix errors in DB config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,8 @@
 <?php
 
-// config.php contains database credentials
-include __DIR__ . '/../../config.php';
+// load database credentials
+include __DIR__ . '/../config.php';
 
-// PDO database connection
 try {
     $pdo = new PDO(
         "mysql:host=$db_server;dbname=$db_database_name;charset=utf8mb4;port=3306",
@@ -12,11 +11,10 @@ try {
     );
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // disable emulated prepared statements
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 } catch (PDOException $e) {
     die("connection failed: " . $e->getMessage());
 }
 
-?>  
+?>
