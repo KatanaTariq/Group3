d8a9b6decb6773b2acf0e9782a0a96eac469f0b8
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2025-11-27 17:49:26 +0000

    Change index pathing

--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
 // Create database connection
-include __DIR__ . "/config/database.php";
+include __DIR__ . "/../config/database.php";
 
 // Load the controllers
-require __DIR__ . '/src/controller/BaseController.php';
-require __DIR__ . '/src/controller/AdminController.php';
-require __DIR__ . '/src/controller/AuthController.php';
-require __DIR__ . '/src/controller/BasketController.php';
-require __DIR__ . '/src/controller/CustomerController.php';
-require __DIR__ . '/src/controller/OrderController.php';
-require __DIR__ . '/src/controller/ProductController.php';
-require __DIR__ . '/src/controller/ReturnController.php';
-require __DIR__ . '/src/controller/ReviewController.php';
-require __DIR__ . '/src/controller/WishlistController.php';
+require __DIR__ . '/../src/controller/BaseController.php';
+require __DIR__ . '/../src/controller/AdminController.php';
+require __DIR__ . '/../src/controller/AuthController.php';
+require __DIR__ . '/../src/controller/BasketController.php';
+require __DIR__ . '/../src/controller/CustomerController.php';
+require __DIR__ . '/../src/controller/OrderController.php';
+require __DIR__ . '/../src/controller/ProductController.php';
+require __DIR__ . '/../src/controller/ReturnController.php';
+require __DIR__ . '/../src/controller/ReviewController.php';
+require __DIR__ . '/../src/controller/WishlistController.php';
 
 // Get the current request URL
 $request = $_SERVER['REQUEST_URI'];
@@ -29,16 +29,15 @@ switch ($requestPath) {
     // Home page
     case '/':
     case '/home':
-        require __DIR__ . '/src/view/pages/home.php';
+        require __DIR__ . '/../src/view/pages/home.php';
         break;
     // Login page
     case '/login':
-        require __DIR__ . '/src/view/pages/login.php';
+        require __DIR__ . '/../src/view/pages/login.php';
         break;
     // 404 fallback
     default:
         http_response_code(404);
-        require __DIR__ . '/src/view/pages/404.php';
+        require __DIR__ . '/../src/view/pages/404.php';
         break;
 }
-?>
