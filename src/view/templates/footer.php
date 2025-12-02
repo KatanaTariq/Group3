f722317c492e6453d045c4431fac6da3b5169f57
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2025-12-02 00:37:58 +0000

    Ensure womespage is CSP compliant

---
+++ src/view/templates/footer.php
@@ -0,0 +1,18 @@
+<footer id="site-footer">
+    <div class="footer-nav">
+        <h3>Quick Links</h3>
+        <a href="/home">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/login">Sign In</a>
+    </div>
+
+    <div class="footer-center">
+        <p>Let's stay in touch! Sign up to experience the benefits of Athletiq!</p>
+        <a href="/login"><button class="footer-signup-btn">Sign Up</button></a>
+    </div>
+
+    <div class="footer-logo">
+        <img src="/src/view/images/logos/athletiq_logo_transparent.png" alt="Athletiq Logo">
+    </div>
+</footer>
