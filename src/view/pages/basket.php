fd9d243144a56aae4b12c3e0e173dc1ffd91cf1b
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2025-12-03 00:58:58 +0000

    Split basket into .css and .php

--- src/view/pages/basket.php
+++ src/view/pages/basket.php
@@ -1,14 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Athletiq | Basket</title>
+    <link rel="stylesheet" href="/src/view/css/nav.css">
+    <link rel="stylesheet" href="/src/view/css/footer.css">
+    <link rel="stylesheet" href="/src/view/css/basket.css">
+</head>
+
+<body>
+
 <?php include __DIR__ . '/../templates/nav.php'; ?>
 
 <h1 class="basket-title">Your Shopping Basket</h1>
 
-<div class="basket-container" id="basket-items"></div>
+<div class="basket-container" id="basket-items">
+    
+</div>
 
 <div class="basket-footer">
     <p class="total">Total: £<span id="basket-total">0.00</span></p>
-    <button class="checkout-btn" onclick="location.href='checkout.html'">Proceed to Checkout</button>
+    <button class="checkout-btn" onclick="location.href='/checkout'">Proceed to Checkout</button>
 </div>
 
 <?php include __DIR__ . '/../templates/footer.php'; ?>
 
 <script src="/src/view/js/basket.js"></script>
+
+</body>
+</html>
