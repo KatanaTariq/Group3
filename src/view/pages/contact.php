c16f55c2feb85a295a57a2f7fdab6aa2ec13f41f
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2025-12-04 17:00:15 +0000

    Make all frontend pages consistent and nice to look at, update index

--- src/view/pages/contact.php
+++ src/view/pages/contact.php
@@ -16,27 +16,30 @@
     
     <?php include __DIR__ . '/../templates/nav.php'; ?>
 
-    <!-- contact us -->
-     <div class="contactUsPage">
+    <div class="contactUsPage">
+
         <div class="header">
             <h1>Contact Us</h1>
         </div>
+
         <div class="container">
+
             <div class="contact_details">
                 <h2>Our contact details</h2>
 
-                <i class='bx  bx-location'></i> 
+                <i class='bx bx-location'></i> 
                 <h3 class="header3">Address</h3>
-                <p> Aston University
-                    <br>Birmingham
-                    <br>B4 7ET
+                <p>
+                    Aston University<br>
+                    Birmingham<br>
+                    B4 7ET
                 </p>
 
-                <i class='bx  bx-envelope'></i> 
+                <i class='bx bx-envelope'></i> 
                 <h3 class="header3">Email</h3>
                 <p>Athletiq@aston.ac.uk</p>
 
-                <i class='bx  bx-phone'></i> 
+                <i class='bx bx-phone'></i> 
                 <h3 class="header3">Phone</h3>
                 <p>0121 204 4007</p>
             </div>
@@ -45,23 +48,31 @@
                 <h2>Write your inquiry</h2>
                 <p>Need to get in touch? Fill out this form to send us a message and we'll lend you a hand</p>
 
-                <form action="">
+                <form method="post" action="/contact">
+
                     <div class="input-box">
-                        <input type="text" placeholder="Enter your full name" required>
+                        <input type="text" name="name" placeholder="Enter your full name" required>
                     </div>
+
                     <div class="input-box">
-                        <input type="text" placeholder="Enter your email" required>
+                        <input type="email" name="email" placeholder="Enter your email" required>
                     </div>
+
                     <div class="message-box">
-                        <textarea name="text" placeholder="Enter your message"></textarea>
+                        <textarea name="message" placeholder="Enter your message" required></textarea>
                     </div>
+
+                    <div class="submit">
+                        <button type="submit" class="send_message">Submit</button> 
+                    </div>
+
                 </form>
-                <div class="submit">
-                    <button type="submit" class="send_message">Submit</button> 
-                </div>
+
             </div>
+
         </div>
-     </div>
+
+    </div>
 
     <?php include __DIR__ . '/../templates/footer.php'; ?>
 
