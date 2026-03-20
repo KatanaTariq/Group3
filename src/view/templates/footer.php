db93d7c6c3e8cf55bed5abd3c338379a485c21f5
Kiera Thomas-Cooper <kieratcoops@outlook.com>
2026-03-20 15:36:33 +0000

    Provisional bug fixes

--- src/view/templates/footer.php
+++ src/view/templates/footer.php
@@ -1,28 +1,25 @@
 <footer id="site-footer">
     <div class="footer-nav">
         <h3>Quick Links</h3>
-        <a href="<?= BASE_URL ?>/home">Home</a>
-        <a href="<?= BASE_URL ?>/about">About Us</a>
-        <a href="<?= BASE_URL ?>/contact">Contact Us</a>
-        <a href="<?= BASE_URL ?>/login">Sign In</a>
-        <?php if (($_SERVER['REQUEST_URI'] === BASE_URL . '/home' || $_SERVER['REQUEST_URI'] === '/')): ?>
-            <a href="<?= BASE_URL ?>/admin/login">Admin</a>
+        <a href="/home">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/contact">Contact Us</a>
+        <a href="/login">Sign In</a>
+
+        <?php if ($_SERVER['REQUEST_URI'] === '/home' || $_SERVER['REQUEST_URI'] === '/'): ?>
+            <a href="/admin/login">Admin</a>
         <?php endif; ?>
     </div>
 
     <div class="footer-center">
         <p>Let's stay in touch! Sign up to experience the benefits of Athletiq!</p>
-        <a href="<?= BASE_URL ?>/signup">
+        <a href="/signup">
             <button class="footer-signup-btn">Sign Up</button>
         </a>
     </div>
 
     <div class="footer-logo">
-        <img
-            src="<?= BASE_URL ?>/public/images/logos/athletiq_logo.png"
-            alt="Athletiq Logo"
-            class="logo-img"
-        >
+        <img src="/public/images/logos/athletiq_logo_transparent.png" alt="Athletiq Logo">
     </div>
 </footer>
 
